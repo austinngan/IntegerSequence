7aff477043af79e645d0f49f41a9f0aad0c9e283
austinngan <angan20@stuy.edu>
2020-12-07 13:39:51 -0500

    added first ArraySequence constructor

---
+++ ArraySequence.java
@@ -0,0 +1,9 @@
+import java.util.NoSuchElementException;
+public class ArraySequence implements IntegerSequence{
+  int currentIndex;
+  int[] data;
+
+  public ArraySequence(int[] other){
+    data=other;
+  }
+}
